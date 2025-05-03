e128315c0eae94dd4452e2d244a709c8d647a4f1
Sahil Malhotra <sahil061626@gmail.com>
2025-05-03 19:43:05 +0530

    updated portnumber for angular app

--- BitBasket.API/Program.cs
+++ BitBasket.API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddControllers()
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder => builder
-    .WithOrigins("http://localhost:4132")
+    .WithOrigins("http://localhost:4200")
     .AllowAnyHeader()
     .AllowAnyMethod()
     );
